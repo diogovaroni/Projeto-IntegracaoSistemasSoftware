e6cf95f4493b3eebacb5fe458c6b9daae957bf45
Varoni <varonidiogo@gmail.com>
2023-04-09 22:56:58 -0300

    Adição do serviço de DB no arquivo Startup.cs utilizando a string de conexão 'DefaultConnection'

--- api_curso_cs/Startup.cs
+++ api_curso_cs/Startup.cs
@@ -1,3 +1,4 @@
+using api_curso_cs.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace api_curso_cs
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CursoContext>(options =>
+                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
